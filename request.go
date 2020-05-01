e9dd1b0b9bfef908ab954856f4a0f586865a5ee8
Frank Xu <frank.xu@northbright.com>
2020-05-01 16:06:50 +0800

    Remove *Request.String() and *Request.id()

--- request.go
+++ request.go
@@ -2,8 +2,6 @@ package filehashes
 
 import (
 	"crypto"
-	"crypto/md5"
-	"fmt"
 )
 
 // Request represents the request of sum a single file.
@@ -19,25 +17,4 @@ func NewRequest(file string, hashAlgs []crypto.Hash) *Request {
 		hashAlgs = DefaultHashAlgs
 	}
 	return &Request{file, hashAlgs}
-}
-
-// String returns a formated string for request.
-func (req *Request) String() string {
-	str := fmt.Sprintf("file: %s(hash algs:", req.File)
-	for _, h := range req.HashAlgs {
-		str += fmt.Sprintf(" %v", h)
-	}
-	str += ")"
-	return str
-}
-
-// id returns the request's ID.
-// It uses md5 hash of file name and hash algorithms as the unique request ID.
-func (req *Request) id() string {
-	h := md5.New()
-	h.Write([]byte(req.File))
-	for _, alg := range req.HashAlgs {
-		h.Write([]byte(fmt.Sprintf("%d", alg)))
-	}
-	return fmt.Sprintf("%X", h.Sum(nil))
 }
