8aa51e8b8489be7de5e58d771ce9b52ebda502d0
Frank Xu <frank.xu@northbright.com>
2020-05-17 16:58:15 +0800

    Make it possible to cancel the task even it's scheduled

--- manager.go
+++ manager.go
@@ -1,7 +1,15 @@
 package filehashes
 
 import (
+	"bufio"
 	"context"
+	"crypto"
+	"encoding"
+	"errors"
+	"fmt"
+	"hash"
+	"io"
+	"os"
 )
 
 // Manager controls the number of concurrent worker goroutines which compute files hashes.
@@ -10,6 +18,35 @@ type Manager struct {
 	bufferSize  int
 	sem         chan struct{}
 	ch          chan *Message
+}
+
+var (
+	DefaultConcurrency = 4
+	DefaultBufferSize  = 8 * 1024 * 1024
+
+	ErrNoFileToHash         = errors.New("no file to hash")
+	ErrNoHashFuncs          = errors.New("no hash functions")
+	ErrHashFuncNotAvailable = errors.New("hash function is not available")
+	ErrInvalidHashState     = errors.New("invalid hash state")
+	ErrFileIsDir            = errors.New("file is dir")
+)
+
+func openFile(file string) (*os.File, os.FileInfo, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if fi.IsDir() {
+		return nil, nil, ErrFileIsDir
+	}
+
+	return f, fi, nil
 }
 
 // NewManager creates a new manager and returns a channel to receive the messages.
@@ -33,17 +70,160 @@ func NewManager(concurrency int, bufferSize int) (*Manager, <-chan *Message) {
 	return m, m.ch
 }
 
-// Start starts to sum files by given requests.
+// Start starts to sum files by given request.
 // Caller should import hash function packages for the hash functions.
 // e.g. import (_ "crypto/md5")
-func (m *Manager) Start(ctx context.Context, reqs []*Request) {
-	for _, req := range reqs {
-		go func(req *Request) {
-			m.ch <- newMessage(SCHEDULED, req, nil)
+func (m *Manager) Start(ctx context.Context, req *Request) {
+	go func() {
+		m.sum(ctx, req)
+	}()
+}
 
-			m.sem <- struct{}{}
-			sum(ctx, m.bufferSize, req, m.ch)
-			<-m.sem
-		}(req)
+func (m *Manager) sum(ctx context.Context, req *Request) {
+	defer func() {
+		<-m.sem
+	}()
+
+	m.ch <- newMessage(SCHEDULED, req, nil)
+
+BEFORE_START:
+	// This loop make it possible to cancel the task even it's scheduled:
+	// blocked at m.mem <- struct{}{}.
+	for {
+		select {
+		case <-ctx.Done():
+			// Stopped before start, use previous request.
+			resumeReq := req
+			// Send stopped message.
+			m.ch <- newMessage(STOPPED, req, resumeReq)
+			return
+		case m.sem <- struct{}{}:
+			break BEFORE_START
+		}
 	}
+
+	// Check hash functions.
+	if len(req.HashFuncs) == 0 {
+		m.ch <- newMessage(ERROR, req, ErrNoHashFuncs.Error())
+		return
+	}
+
+	// Create hash map.
+	// Key is the crypto.Hash(hash function).
+	// Value is the hash.Hash.
+	hashes := map[crypto.Hash]hash.Hash{}
+	for _, h := range req.HashFuncs {
+		if !h.Available() {
+			m.ch <- newMessage(ERROR, req, ErrHashFuncNotAvailable.Error())
+			return
+		}
+		hashes[h] = h.New()
+	}
+
+	// Open file.
+	f, fi, err := openFile(req.File)
+	if err != nil {
+		m.ch <- newMessage(ERROR, req, err.Error())
+		return
+	}
+	defer f.Close()
+
+	// Create bufio.Reader.
+	r := bufio.NewReaderSize(f, m.bufferSize)
+	buf := make([]byte, m.bufferSize)
+
+	size := fi.Size()
+	summedSize := int64(0)
+	oldProgress := 0
+	progress := 0
+
+	// Restore previous hash states.
+	if req.Stat != nil {
+		if !req.StateIsValid() {
+			m.ch <- newMessage(ERROR, req, ErrInvalidHashState.Error())
+			return
+		}
+
+		// Restore saved hash states
+		for k, v := range hashes {
+			u := v.(encoding.BinaryUnmarshaler)
+			data := req.Stat.Datas[k]
+
+			if err := u.UnmarshalBinary(data); err != nil {
+				m.ch <- newMessage(ERROR, req, err.Error())
+				return
+			}
+		}
+
+		// Seek file offset and update summed size.
+		summedSize = req.Stat.SummedSize
+		if _, err := f.Seek(summedSize, os.SEEK_SET); err != nil {
+			m.ch <- newMessage(ERROR, req, err.Error())
+			return
+		}
+
+		m.ch <- newMessage(RESTORED, req, req.Stat)
+
+		// Update progress.
+		progress = req.Stat.Progress
+		oldProgress = progress
+		m.ch <- newMessage(PROGRESSUPDATED, req, progress)
+	} else {
+		// Send sum started message.
+		m.ch <- newMessage(STARTED, req, nil)
+	}
+
+LOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			// Save state and create a request to resume.
+			state := newState(summedSize, progress, hashes)
+			resumeReq := &Request{req.File, req.HashFuncs, state}
+			// Send stopped message.
+			m.ch <- newMessage(STOPPED, req, resumeReq)
+			return
+		default:
+			n, err := r.Read(buf)
+			if err != nil && err != io.EOF {
+				// Send error message.
+				m.ch <- newMessage(ERROR, req, err.Error())
+				return
+			}
+
+			if n == 0 {
+				break LOOP
+			}
+
+			// Adds more data to the running hash.
+			for _, h := range hashes {
+				if n, err = h.Write(buf[:n]); err != nil {
+					return
+				}
+			}
+
+			summedSize += int64(n)
+			if size != 0 {
+				progress = int(float64(summedSize) / float64(size) * 100)
+			}
+			if progress != oldProgress {
+				oldProgress = progress
+				// Send progress updated message.
+				m.ch <- newMessage(PROGRESSUPDATED, req, progress)
+			}
+		}
+	}
+
+	// Done. Send the checksums.
+	checksums := map[crypto.Hash]string{}
+	for k, v := range hashes {
+		checksums[k] = fmt.Sprintf("%X", v.Sum(nil))
+	}
+
+	// Update progress for 0-size file.
+	if size == 0 {
+		m.ch <- newMessage(PROGRESSUPDATED, req, 100)
+	}
+
+	m.ch <- newMessage(DONE, req, checksums)
 }
--- manager_test.go
+++ manager_test.go
@@ -6,17 +6,16 @@ import (
 	_ "crypto/md5"
 	_ "crypto/sha1"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/northbright/filehashes"
 )
 
 func ExampleManager_Start() {
-	ctx := context.Background()
-
 	// You may set the number of concurrent worker goroutines.
 	// It's filehashes.DefaultConcurrency by default.
-	concurrency := 1
+	concurrency := 2
 	bufferSize := filehashes.DefaultBufferSize
 
 	man, ch := filehashes.NewManager(concurrency, bufferSize)
@@ -24,21 +23,37 @@ func ExampleManager_Start() {
 	// Create requests.
 	reqs := []*filehashes.Request{
 		filehashes.NewRequest("README.md", []crypto.Hash{crypto.MD5}, nil),
-		filehashes.NewRequest("filehashes.go", []crypto.Hash{crypto.SHA1}, nil),
-		filehashes.NewRequest("go.mod", []crypto.Hash{crypto.MD5, crypto.SHA1}, nil),
+		filehashes.NewRequest("manager.go", []crypto.Hash{crypto.SHA1}, nil),
+		filehashes.NewRequest("manager_test.go", []crypto.Hash{crypto.MD5, crypto.SHA1}, nil),
 		filehashes.NewRequest("go.sum", []crypto.Hash{crypto.MD5, crypto.SHA1}, nil),
 	}
 
-	// Start to sum files.
-	man.Start(ctx, reqs)
+	// Use a timeout to exit the program.
+	tm := time.After(5 * time.Second)
 
-	// Create a timeout to exit.
-	chTimeout := time.After(5 * time.Second)
+	// Record the cancel func for each request's file.
+	cancelMap := map[string]context.CancelFunc{}
+
+	for _, req := range reqs {
+		ctx, cancel := context.WithCancel(context.Background())
+		cancelMap[req.File] = cancel
+		// Start to sum files.
+		man.Start(ctx, req)
+	}
+
+	// Stop 1 task randomly.
+	// The stopped task will be resume with previous progress in message loop.
+	go func() {
+		time.Sleep(1 * time.Millisecond)
+		i := rand.Intn(len(reqs))
+		cancel := cancelMap[reqs[i].File]
+		cancel()
+	}()
 
 	// Consume messages.
 	for {
 		select {
-		case <-chTimeout:
+		case <-tm:
 			log.Printf("timeout")
 			return
 		case m := <-ch:
@@ -46,13 +61,23 @@ func ExampleManager_Start() {
 			case filehashes.ERROR,
 				filehashes.SCHEDULED,
 				filehashes.STARTED,
-				filehashes.STOPPED,
 				filehashes.RESTORED,
 				filehashes.PROGRESSUPDATED,
 				filehashes.DONE:
 				// All messages can be marshaled to JSON.
 				buf, _ := m.JSON()
 				log.Printf("message: %v", string(buf))
+
+			case filehashes.STOPPED:
+				buf, _ := m.JSON()
+				log.Printf("message: %v", string(buf))
+
+				// Resume the task if it's stopped.
+				resumeReq := m.Data.(*filehashes.Request)
+				ctx, cancel := context.WithCancel(context.Background())
+				cancelMap[resumeReq.File] = cancel
+
+				man.Start(ctx, resumeReq)
 
 			default:
 				log.Printf("unknown message: %v", m)
